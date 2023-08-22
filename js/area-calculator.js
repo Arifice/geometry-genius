f91e0737b2ccc5647615ff948d38c353a9e28c72
Arifice <arifice13@gmail.com>
2023-08-22 14:03:59 +0600

    added

--- js/area-calculator.js
+++ js/area-calculator.js
@@ -1,6 +1,10 @@
 function calculateTriangleArea(){
     const base=getInputValue('triangle-base');
     const height=getInputValue('tirangle-height');
+    if(isNaN(base)||isNaN(height)){
+        alert('Please input number');
+        return;
+    }
     const area=.5*base*height;    
     setElementInnerText('triangle-area',area);
 }
@@ -8,32 +12,52 @@ function calculateTriangleArea(){
 function calculateRectangleArea(){
     const length=getInputValue('rectangle-base');
     const  height=getInputValue('rectangle-height');
+    if(isNaN(length)||isNaN(height)){
+        alert('Please input number');
+        return;
+    }
     const area=length*height;
     setElementInnerText('rectangle-area',area);
 }
 function calculateParallelogramArea(){
     const base=getInputValue('parallelogram-base');
     const height=getInputValue('parallelogram-height');
+    if(isNaN(base)||isNaN(height)){
+        alert('Please input number');
+        return;
+    }
     const area=base*height;
     setElementInnerText('parallelogram-area',area);
 }
 function calculateRhombusleArea(){
     const length=getInputValue('first-diagonal');
     const height=getInputValue('second-diagonal');
+    if(isNaN(length)||isNaN(height)){
+        alert('Please input number');
+        return;
+    }
     const area=.5*length*height;
     setElementInnerText('rhombus-area',area);
 }
 function calculatePentagonArea(){
-    const length=getInputValue('pentagon-perimeter');
+    const perimeter=getInputValue('pentagon-perimeter');
     const  height=getInputValue('pentagon-height');
-    const area=.5*length*height;
+    if(isNaN(perimeter)||isNaN(height)){
+        alert('Please input number');
+        return;
+    }
+    const area=.5*perimeter*height;
     setElementInnerText('pentagon-area',area);
 
 }
 function calculateEllipseArea(){
-    const length=getInputValue('ellipse-base');
-    const  height=getInputValue('ellipse-height');
-    const area=Math.PI*length*height;
+    const radious1=getInputValue('ellipse-base');
+    const  radious2=getInputValue('ellipse-height');
+    if(isNaN(radious1)||isNaN(radious2)){
+        alert('Please input number');
+        return;
+    }    
+    const area=Math.PI*radious1*radious2;
     const areaTwoDecimal=area.toFixed(3);
     setElementInnerText('ellipse-area',areaTwoDecimal);
 }
@@ -41,10 +65,7 @@ function calculateEllipseArea(){
 function getInputValue(fieldId){    
     const inputField=document.getElementById(fieldId);
     const input=inputField.value;
-    if(isNaN(input)){
-        alert('Invalid Input');
-        return;
-    }
+    inputField.value='';    
     return parseFloat(input);
 }
 function setElementInnerText(elementId,area){
